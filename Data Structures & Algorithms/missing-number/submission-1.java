ec30f7fd64203599dd64915754cdb97833dd847e
RashmiBansal-30 <bansal35indrashmi@gmail.com>
2026-07-05 22:47:18 +0530

    Add: missing-number - submission-1

---
+++ Data Structures & Algorithms/missing-number/submission-1.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int l = nums.length;
+        for (int i = 0; i < nums.length; i++){
+            l ^=  i ^ nums[i];
+        }
+
+        return l;
+    }
+}
