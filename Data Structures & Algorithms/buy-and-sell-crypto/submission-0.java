39926615621bb7224b682fd3536d1881a328dff4
RashmiBansal-30 <bansal35indrashmi@gmail.com>
2026-07-07 14:44:26 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length <= 1){
+            return 0;
+        }
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++){
+            if (prices[i] < minPrice){
+                minPrice = prices[i];
+            }
+            else{
+                int currentProfit = prices[i] - minPrice;
+                maxProfit = Math.max(maxProfit, currentProfit);
+            }
+        }
+        return maxProfit;
+    }
+}
